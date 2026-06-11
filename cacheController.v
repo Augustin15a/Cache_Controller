031e81ea0b1895f5bb09afc4c0fdcecf790de771
Augustin15a <92544752+Augustin15a@users.noreply.github.com>
2026-06-11 22:13:10 +0300

    inainteDeFinal

--- cacheController.v
+++ cacheController.v
@@ -6,11 +6,59 @@ module cacheController(
 	input clk,
 	input reset,
 	input [31:0]address,
-	//semnale de la cache si spre
+	
+//semnale de la cache si spre COMPARE_TAG state
 	input [19*4 - 1:0]tags_from_cache,
 	input tags_delivered,
 	output request_memory_tags,
-	output [6:0]index
+	output [6:0]index,
+	
+//semnale de la cache si spre Read HIT state
+	//input de la cache
+	input block_delivered,//cache ul imi spune daca a trimis| 1  = a trimis
+	input [511:0]block_data,
+	
+	//output catre cache
+	output request_block,//cer de la cache sa mi trimita blockul
+	output [1:0]way_out,//al catelea din cele 4
+	
+	//output catre procesor
+	output [31:0]data_for_cpu,//data ceruta de cpu
+	output data_ready,
+
+//semnale de la cache si ram cand Read MISS state
+	//cache
+	input newBlock_delivered,
+	output send_block_from_ram,
+	output [511:0]block_data_ram_to_cache,
+	output [18:0]tag_to_cache,
+	output [6:0]index_to_cache,
+	output [1:0]way_to_cache,
+	//ram
+	input block_delivered_ram,
+	input [511:0]block_data_ram,
+	output [31:0]memory_address_to_ram,
+	output request_block_ram,
+
+//semnale  WRITE HIT state
+	//catre cache
+	//index ul il pastrez de la starea READ MISS si way
+	input write_done_cache,
+	output write_hit_semnal_cache,
+	output [31:0]data_cache,
+	output dirty_bit,
+	output [3:0]word_offset,
+	
+	//de la cpu
+	input [31:0]data_for_cpu_write,
+	output write_done_cpu,
+//semnale pentru starea EVICT și WRITE MISS
+	//de la cache
+	input [1:0] lru_way,
+	input victim_dirty,
+	input [18:0] victim_tag,
+	// către RAM
+	output request_write_ram
 );
 //								States
 //		-IDLE       	000	Waiting for a new request
@@ -40,7 +88,6 @@ register #(
 	.write_data(clk),
 	.out_reg(out_reg_current_state)
 );
-wire evict = 1'b0;
 //Reset Logic
 wire neg_reset;
 not(neg_reset,reset);
@@ -53,13 +100,16 @@ not(neg_reset,reset);
 and(data_current_state[0],data_next_state[0],neg_reset);
 and(data_current_state[1],data_next_state[1],neg_reset);
 and(data_current_state[2],data_next_state[2],neg_reset);
-
+//stari declarate ca wire
+wire read_miss_state;
+wire final_request_block_ram;
+wire final_send_block_from_ram;
 
 //										IDLE STATE
 
-wire idle_state;//1 - esti in starea idle
 //VERIFICARE STARE
 //in tabelul portii nor doar cand input e 0 0 outputul e 1
+wire idle_state;
 nor(idle_state, out_reg_current_state[0], out_reg_current_state[1], out_reg_current_state[2]);
 //salvez adresa si o impart
 wire [31:0]wire_address;
@@ -67,14 +117,29 @@ wire [5:0]wire_offset;
 wire [6:0]wire_index;
 wire [18:0]wire_tag;
 
+wire load_address;
+and(load_address, idle_state, cpu_request);
+genvar i,j;
+wire [31:0] address_mux_out;
+generate
+    for(i = 0; i < 32; i = i + 1)
+        begin : addr_mux
+            MUX2to1 mux_addr(
+                .x(wire_address[i]),
+                .y(address[i]),
+                .sel(load_address),
+                .out(address_mux_out[i])
+            );
+        end
+endgenerate
+
 register #(
 	.size_reg(32)
-)address_reg(
-	.data(address),
-	.write_data(idle_state),
-	.out_reg(wire_address)
+) address_reg(
+    .data(address_mux_out),
+    .write_data(clk),
+    .out_reg(wire_address)
 );
-genvar i;
 generate
 	for(i = 0;i < 6; i = i + 1)
 		begin : offsetblock
@@ -120,13 +185,13 @@ and(request_memory_tags,compare_tag_state,neg_tags_delivered);
 //trimit index la cache doar daca request_memory_tags e 1 altfel X
 generate 
 	for(i = 0; i < 7; i = i + 1)
-		begin : equal
+		begin : send_to_cache1
 			bufif1(index[i],wire_index[i],request_memory_tags);
 		end
 endgenerate
 
 //split input tags_from_cache in 4
-wire [18:0]tag_compare_in[0:3];
+wire [18:0]tag_compare_in[3:0];
 generate 
 	for(i = 0; i < 19; i = i + 1)
 		begin : split_tags
@@ -171,6 +236,32 @@ Comparator #(
 	.result(out_comparatoare[3])
 );
 
+//mentinerea wayului pentru viitoare stari
+wire [3:0]out_reg_comparatoare;
+wire load_way;
+and(load_way, compare_tag_state, tags_delivered);
+
+wire [3:0] mux_way_out;
+generate
+    for(i = 0; i < 4; i = i + 1)
+        begin : way_mux_block
+            MUX2to1 mux_w(
+                .x(out_reg_comparatoare[i]),
+                .y(out_comparatoare[i]),
+                .sel(load_way),
+                .out(mux_way_out[i])
+            );
+        end
+endgenerate
+register #(
+    .size_reg(4)
+) way_hit_reg (
+    .data(mux_way_out),
+    .write_data(clk),
+    .out_reg(out_reg_comparatoare)
+);
+
+//logica de gasire a tagului cerut
 wire result_comparatoare;
 or(result_comparatoare,
 	out_comparatoare[0],
@@ -199,34 +290,223 @@ and(stay_compare_tag,neg_tags_delivered,compare_tag_state);
 //										READ HIT
 //VERIFICARE STARE
 wire read_hit_state;
-
 and(
 	read_hit_state,//out
 	neg_out_reg_current_state[2],
 	out_reg_current_state[1],
 	neg_out_reg_current_state[0]
 );
+//encoder pentru way_out
+
+wire out_or1;
+wire out_or2;
+wire [1:0]wire_way;
+or(out_or1, out_reg_comparatoare[1], out_reg_comparatoare[3]);
+or(out_or2, out_reg_comparatoare[2], out_reg_comparatoare[3]);
+
+buf(wire_way[0], out_or1);
+buf(wire_way[1], out_or2);
+
+//cer cacheului blockul
+wire neg_block_delivered;
+not(neg_block_delivered,block_delivered);
+and(request_block,read_hit_state,neg_block_delivered);
+//trimit indexul si way ul
+generate 
+	for(i = 0; i < 7; i = i + 1)
+		begin : send_to_cache2
+			bufif1(index[i],wire_index[i],request_block);
+		end
+endgenerate
+bufif1(way_out[0], wire_way[0], read_hit_state);
+bufif1(way_out[1], wire_way[1], read_hit_state);
+
+//				LOGICA IMPLEMENTATA SI LA HIT SI MISS
+//la miss trimit la cache blockul de date si ar trebuii sa revin in starea hit pentru a
+//cere de la cache block ul pe care doar ce l am avut, asa ca in starea read miss cand il am 
+//il trimit la cache si data de care am nevoie la cpu
+//logica pt alegerea wordului din block.O sa fac mai multe muxuri care compara 2 numere a cate 32
+//biti pe 4 nivele, select ul va fi wire_offset[nivel + 2  pentru ca primii 2 biti din wire_offset 
+//sunt cei  byte offset]
+
+//alegem blocul pe care sa l comparam
+wire [511:0]block_input;
+MUX2to1_param #(.size_data(512)) mux_block_select(
+    .x(block_data),        // de la cache - READ HIT
+    .y(block_data_ram),    // de la RAM   - READ MISS
+    .sel(read_miss_state), // 0 = HIT, 1 = MISS
+    .out(block_input)
+);
+wire [31:0]mux_out[14:0];
+
+generate
+    for(i = 1; i <= 4; i = i + 1)
+        begin : nivel
+            for(j = 1; j <= 16 >> i; j = j + 1)
+                begin : mux_nivel
+					if(i == 1)
+						begin
+							MUX2to1_param #(.size_data(32)) mux_level(
+								.x(block_input[(32 * (2 * j - 1)) - 1 : 32 * (2 * j - 2)]),
+								.y(block_input[(32 * (2 * j)) - 1 : 32 * (2 * j - 1)]),
+								.sel(wire_offset[i + 1]),
+								.out(mux_out[j - 1])
+							);
+						end
+					else
+						begin
+                            MUX2to1_param #(.size_data(32)) mux_level(
+                                .x(mux_out[(16 - (16 >> (i-2))) + 2*j - 2]),
+                                .y(mux_out[(16 - (16 >> (i-2))) + 2*j - 1]),
+                                .sel(wire_offset[i + 1]),
+                                .out(mux_out[(16 - (16 >> (i-1))) + j - 1])
+                            );
+                        end
+                end
+        end
+endgenerate
+//logica de trimitere a wordului cerut de cpu
+//datele se trimit doar atunci cand ma aflu in read hit state si blockul a fost trimis 
+//de cache la mine pentru a selecta wordul cerut
+wire data_ready_hit;
+wire data_ready_miss;
+
+and(data_ready_hit, read_hit_state, block_delivered);
+and(data_ready_miss, read_miss_state, block_delivered_ram);
+or(data_ready, data_ready_hit, data_ready_miss);
+
+generate
+	for(i = 0;i < 32;i = i + 1)
+		begin : buf_mux_out_and_data_for_cpu
+			bufif1(data_for_cpu[i],mux_out[14][i],data_ready);
+		end
+endgenerate
+//conditia de a ramane in stare
+wire neg_data_ready;
+not(neg_data_ready,data_ready);
+
+wire stay_read_hit;
+and(stay_read_hit, read_hit_state, neg_data_ready,neg_block_delivered);
+//logica pentru a merge in idle
+wire back_to_idle_from_read_hit;
+and(back_to_idle_from_read_hit,read_hit_state, data_ready, block_delivered);
 
 //										READ MISS
 //VERIFICARE STARE
-wire read_miss_state;
 and(
 	read_miss_state,//out
 	neg_out_reg_current_state[2],
 	out_reg_current_state[1],
 	out_reg_current_state[0]
 );
+//logica pentru a trimite adresa catre ram pentru a primi blockul
+
+wire neg_block_delivered_ram;
+not(neg_block_delivered_ram, block_delivered_ram);
+and(request_block_ram, read_miss_state, neg_block_delivered_ram);
+
+generate
+    for(i = 0; i < 32; i = i + 1)
+        begin : send_address_to_ram
+            bufif1(memory_address_to_ram[i], wire_address[i], final_request_block_ram);
+        end
+endgenerate
+
+//ram a trimis blockul
+wire neg_newBlock_delivered;
+not(neg_newBlock_delivered,newBlock_delivered);
+and(send_block_from_ram, read_miss_state, block_delivered_ram,neg_newBlock_delivered);
+
+//trimite blockul la cache
+generate
+    for(i = 0; i < 512; i = i + 1)
+        begin : send_block_to_cache
+            bufif1(block_data_ram_to_cache[i], block_data_ram[i], final_send_block_from_ram);
+        end
+endgenerate
+
+generate
+    for(i = 0; i < 19; i = i + 1)
+        begin : send_tag_to_cache
+            bufif1(tag_to_cache[i], wire_tag[i], final_send_block_from_ram);
+        end
+endgenerate
+
+generate
+    for(i = 0; i < 7; i = i + 1)
+        begin : send_index_to_cache
+            bufif1(index_to_cache[i], wire_index[i], final_send_block_from_ram);
+        end
+endgenerate
+
+bufif1(way_to_cache[0],lru_way[0], final_send_block_from_ram);
+bufif1(way_to_cache[1],lru_way[1], final_send_block_from_ram);
+
+//tranzitiile read miss 
+//ramane in stare
+wire stay_read_miss;
+and(stay_read_miss, read_miss_state, neg_newBlock_delivered);
+//trece la idle
+wire back_to_idle_from_read_miss;
+and(back_to_idle_from_read_miss, read_miss_state, newBlock_delivered);
 
 //										WRITE HIT
 //VERIFICARE STARE
 wire write_hit_state;
-
 and(
 	write_hit_state,//out
 	out_reg_current_state[2],
 	neg_out_reg_current_state[1],
 	neg_out_reg_current_state[0]
 );
+//logica pentru a trimite adresa catre cache data
+wire neg_write_done_cache;
+not(neg_write_done_cache, write_done_cache);
+and(write_hit_semnal_cache,write_hit_state,neg_write_done_cache);
+
+//index
+generate
+    for(i = 0; i < 7; i = i + 1)
+        begin : send_index_write_hit
+            bufif1(index[i], wire_index[i], write_hit_semnal_cache);
+        end
+endgenerate
+
+//way
+bufif1(way_out[0], wire_way[0], write_hit_semnal_cache);
+bufif1(way_out[1], wire_way[1], write_hit_semnal_cache);
+
+//word offset
+generate
+    for(i = 0; i < 4; i = i + 1)
+        begin : send_word_offset_write_hit
+            bufif1(word_offset[i], wire_offset[i + 2], write_hit_semnal_cache);
+        end
+endgenerate
+
+//dirty bit
+bufif1(dirty_bit, 1'b1, write_hit_semnal_cache);
+
+//data de la cpu
+generate
+    for(i = 0; i < 32; i = i + 1)
+        begin : send_data_write_hit
+            bufif1(data_cache[i], data_for_cpu_write[i], write_hit_semnal_cache);
+        end
+endgenerate
+
+// write done catre cpu
+and(write_done_cpu, write_hit_state, write_done_cache);
+
+//conditia de a ramane in stare
+wire neg_write_done_cpu;
+not(neg_write_done_cpu,write_done_cpu);
+wire stay_write_hit;
+and(stay_write_hit, write_hit_state, neg_write_done_cpu);
+
+// back to idle
+wire back_to_idle_from_write_hit;
+and(back_to_idle_from_write_hit, write_hit_state, write_done_cache);
 
 //										WRITE MISS
 //VERIFICARE STARE
@@ -238,6 +518,28 @@ and(
 	neg_out_reg_current_state[1],
 	out_reg_current_state[0]
 );
+// Cerem blocul din RAM
+wire request_block_ram_write_miss;
+and(request_block_ram_write_miss, write_miss_state, neg_block_delivered_ram);
+
+// cerem block de la ram
+or(final_request_block_ram, request_block_ram, request_block_ram_write_miss);
+
+// trimit blockul la cache
+wire send_block_from_ram_write_miss;
+and(send_block_from_ram_write_miss, write_miss_state, block_delivered_ram, neg_newBlock_delivered);
+
+or(final_send_block_from_ram, send_block_from_ram, send_block_from_ram_write_miss);
+
+
+// Tranzitiile stării WRITE MISS
+//ramane in stare
+wire stay_write_miss;
+and(stay_write_miss, write_miss_state, neg_newBlock_delivered);
+
+//merge la idle
+wire back_to_idle_from_write_miss;
+and(back_to_idle_from_write_miss, write_miss_state, newBlock_delivered);
 
 //										  EVICT
 //VERIFICARE STARE
@@ -249,6 +551,44 @@ and(
 	out_reg_current_state[1],
 	neg_out_reg_current_state[0]
 );
+// Cerem scriere în RAM
+and(request_write_ram, evict_state, neg_block_delivered_ram);
+generate
+    for(i = 0; i < 7; i = i + 1)
+        begin : send_index_evict
+            bufif1(index[i], wire_index[i], evict_state);
+        end
+endgenerate
+bufif1(way_out[0], lru_way[0], evict_state);
+bufif1(way_out[1], lru_way[1], evict_state);
+
+// Adresa trimisă la RAM
+generate
+    for(i = 0; i < 6; i = i + 1)
+        begin : evict_addr_offset
+            bufif1(memory_address_to_ram[i], 1'b0, request_write_ram);
+        end
+    for(i = 0; i < 7; i = i + 1)
+        begin : evict_addr_index
+            bufif1(memory_address_to_ram[i + 6], wire_index[i], request_write_ram);
+        end
+    for(i = 0; i < 19; i = i + 1)
+        begin : evict_addr_tag
+            bufif1(memory_address_to_ram[i + 13], victim_tag[i], request_write_ram);
+        end
+endgenerate
+
+// mergem în READ MISS sau WRITE MISS pentru a aduce noul bloc cerut de cpu
+wire evict_done = block_delivered_ram;
+
+wire stay_evict;
+and(stay_evict, evict_state, neg_block_delivered_ram);
+
+wire evict_to_read_miss;
+and(evict_to_read_miss, evict_state, evict_done, read_request);
+
+wire evict_to_write_miss;
+and(evict_to_write_miss, evict_state, evict_done, write_request);
 
 //									TRANSITION STATES
 
@@ -256,35 +596,89 @@ and(
 //adica pentru bitul 0 vedem ca in COMPARE_TAG WRITE MISS si READ MISS observam ca sunt singurele
 //stari unde lsb este 1
 
-//transitia pt bitul 0
-or(data_next_state[0],
-	write_miss,			//trece in stare
-	read_miss,			//trece in stare
-	tranz_compare_tag,	//trece in stare
-	stay_compare_tag,	//pastreaza starea
-	read_miss_state,	//pastreaza starea
-	write_miss_state	//pastreaza starea
+//logica de intoarcere in idle,se intampla la hit si fsm ul revine la starea de asteptare,
+//pentru a face asta o sa am o variabila back_to_idle_from_read_hit care daca e 1 starea se face  000
+
+wire back_to_idle;
+or(
+	back_to_idle,
+	back_to_idle_from_read_hit,
+	back_to_idle_from_read_miss,
+	back_to_idle_from_write_hit,
+	back_to_idle_from_write_miss
 );
 
-//transitia pt bitul 1
-or(
-	data_next_state[1],
-	read_hit,		//trece in stare
-	read_miss,		//trece in stare
-	evict,			//trece in stare
-	read_miss_state,//pastreaza starea
-	read_hit_state,//pastreaza starea
-	evict_state		//pastreaza starea
+wire neg_back_to_idle;
+not(neg_back_to_idle,back_to_idle);
+
+//tranzitii la stari
+wire neg_victim_dirty;
+not(neg_victim_dirty, victim_dirty);
+wire go_to_evict;
+and(
+	go_to_evict,
+	compare_tag_state,
+	tags_delivered,
+	neg_result_comparatoare,
+	victim_dirty
 );
 
-//transitia pt bitul 2
-or(
-	data_next_state[2],
-	write_miss,		//trece in stare
-	write_hit,		//trece in stare
-	evict,			//trece in stare
-	write_miss_state,//pastreaza starea
-	write_hit_state,//pastreaza starea
-	evict_state		//pastreaza starea
+wire go_to_read_miss;
+and(
+	go_to_read_miss,
+	compare_tag_state,
+	tags_delivered,
+	neg_result_comparatoare,
+	read_request,
+	neg_victim_dirty
 );
+
+wire go_to_write_miss;
+and(
+	go_to_write_miss,
+	compare_tag_state,
+	tags_delivered,
+	neg_result_comparatoare,
+	write_request,
+	neg_victim_dirty
+);
+
+//tranzitia pt bitul 0
+wire [2:0]wire_data_next_state;
+or(
+	wire_data_next_state[0],
+	tranz_compare_tag,
+	stay_compare_tag,
+	go_to_read_miss,
+	go_to_write_miss,
+	evict_to_read_miss,
+	evict_to_write_miss,
+	stay_read_miss,
+	stay_write_miss
+);
+and(data_next_state[0],wire_data_next_state[0],neg_back_to_idle);
+//tranzitia pt bitul 1
+or(
+	wire_data_next_state[1],
+	read_hit,
+	go_to_read_miss,
+	go_to_evict,
+	evict_to_read_miss,
+	stay_read_hit,
+	stay_read_miss,
+	stay_evict
+);
+and(data_next_state[1],wire_data_next_state[1],neg_back_to_idle);
+//tranzitia pt bitul 2
+or(
+	wire_data_next_state[2],
+	write_hit,
+	go_to_write_miss,
+	go_to_evict,
+	evict_to_write_miss,
+	stay_write_hit,
+	stay_write_miss,
+	stay_evict
+);
+and(data_next_state[2],wire_data_next_state[2],neg_back_to_idle);
 endmodule
--- test_tb.v
+++ test_tb.v
@@ -1,110 +1,171 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
-module tb_cacheController;
+module cacheController_tb;
 
-reg enable;
-reg cpu_request;
-reg write_request;
-reg read_request;
-reg clk;
-reg reset;
-reg [31:0] data;
+    // --- Semnale de intrare (Regs) ---
+    reg cpu_request;
+    reg write_request;
+    reg read_request;
+    reg clk;
+    reg reset;
+    reg [31:0] address;
+    
+    reg [19*4 - 1:0] tags_from_cache;
+    reg tags_delivered;
+    reg block_delivered;
+    reg [511:0] block_data;
+    reg newBlock_delivered;
+    reg block_delivered_ram;
+    reg [511:0] block_data_ram;
+    reg write_done_cache;
+    reg [31:0] data_for_cpu_write;
+    reg [1:0] lru_way;
+    reg victim_dirty;
+    reg [18:0] victim_tag;
 
-cacheController dut (
-    .enable(enable),
-    .cpu_request(cpu_request),
-    .write_request(write_request),
-    .read_request(read_request),
-    .clk(clk),
-    .reset(reset),
-    .data(data)
-);
+    // --- Semnale de ieșire (Wires) ---
+    wire request_memory_tags;
+    wire [6:0] index;
+    wire request_block;
+    wire [1:0] way_out;
+    wire [31:0] data_for_cpu;
+    wire data_ready;
+    wire send_block_from_ram;
+    wire [511:0] block_data_ram_to_cache;
+    wire [18:0] tag_to_cache;
+    wire [6:0] index_to_cache;
+    wire [1:0] way_to_cache;
+    wire [31:0] memory_address_to_ram;
+    wire request_block_ram;
+    wire write_hit_semnal_cache;
+    wire [31:0] data_cache;
+    wire dirty_bit;
+    wire [3:0] word_offset;
+    wire write_done_cpu;
+    wire request_write_ram;
 
-// Clock 10ns
-initial clk = 0;
-always #5 clk = ~clk;
-
-task show_state;
-    $display("t=%0t | reset=%b cpu=%b rd=%b wr=%b | state=%b (%s)",
-        $time, reset, cpu_request, read_request, write_request,
-        dut.out_reg_current_state,
-        dut.out_reg_current_state == 3'b000 ? "IDLE" :
-        dut.out_reg_current_state == 3'b001 ? "COMPARE" :
-        dut.out_reg_current_state == 3'b010 ? "READ_HIT" :
-        dut.out_reg_current_state == 3'b011 ? "READ_MISS" :
-        dut.out_reg_current_state == 3'b100 ? "WRITE_HIT" :
-        dut.out_reg_current_state == 3'b101 ? "WRITE_MISS" :
-        dut.out_reg_current_state == 3'b110 ? "EVICT" : "UNKNOWN"
+    // --- Instanțierea UUT (Unit Under Test) ---
+    cacheController uut (
+        .cpu_request(cpu_request),
+        .write_request(write_request),
+        .read_request(read_request),
+        .clk(clk),
+        .reset(reset),
+        .address(address),
+        .tags_from_cache(tags_from_cache),
+        .tags_delivered(tags_delivered),
+        .request_memory_tags(request_memory_tags),
+        .index(index),
+        .block_delivered(block_delivered),
+        .block_data(block_data),
+        .request_block(request_block),
+        .way_out(way_out),
+        .data_for_cpu(data_for_cpu),
+        .data_ready(data_ready),
+        .newBlock_delivered(newBlock_delivered),
+        .send_block_from_ram(send_block_from_ram),
+        .block_data_ram_to_cache(block_data_ram_to_cache),
+        .tag_to_cache(tag_to_cache),
+        .index_to_cache(index_to_cache),
+        .way_to_cache(way_to_cache),
+        .block_delivered_ram(block_delivered_ram),
+        .block_data_ram(block_data_ram),
+        .memory_address_to_ram(memory_address_to_ram),
+        .request_block_ram(request_block_ram),
+        .write_done_cache(write_done_cache),
+        .write_hit_semnal_cache(write_hit_semnal_cache),
+        .data_cache(data_cache),
+        .dirty_bit(dirty_bit),
+        .word_offset(word_offset),
+        .data_for_cpu_write(data_for_cpu_write),
+        .write_done_cpu(write_done_cpu),
+        .lru_way(lru_way),
+        .victim_dirty(victim_dirty),
+        .victim_tag(victim_tag),
+        .request_write_ram(request_write_ram)
     );
-endtask
 
-initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars(0, tb_cacheController);
+    // --- Generare Ceas (Perioadă de 10ns) ---
+    always begin
+        #5 clk = ~clk;
+    end
 
-    // Initializare
-    enable = 1; cpu_request = 0;
-    write_request = 0; read_request = 0;
-    data = 32'h0; reset = 1;
-    #30; // 3 clockuri cu reset activ
-    show_state; // expect IDLE
+    // --- Blocul de Stimuli ---
+    initial begin
+        // 1. INIȚIALIZARE STRICTĂ (Eliminăm orice valoare de 'X')
+        clk = 0;
+        reset = 1;
+        cpu_request = 0;
+        write_request = 0;
+        read_request = 0;
+        address = 32'b0;
+        tags_from_cache = {19'b0, 19'b0, 19'b0, 19'b0};
+        tags_delivered = 0;
+        block_delivered = 0;
+        block_data = 512'b0;
+        newBlock_delivered = 0;
+        block_delivered_ram = 0;
+        block_data_ram = 512'b0;
+        write_done_cache = 0;
+        data_for_cpu_write = 32'b0;
+        lru_way = 2'b00;
+        victim_dirty = 0;
+        victim_tag = 19'b0;
 
-    reset = 0;
-    #10;
-    show_state; // expect IDLE
+        // Așteptăm 2 ciclii de ceas complet în starea de Reset
+        #20;
+        
+        // 2. SCOATERE DIN RESET (Pe frontul căzător al ceasului ca să evităm hazardul)
+        @(negedge clk);
+        reset = 0;
+        #10;
 
-    // Test 1 - READ request => trebuie sa mearga in COMPARE
-    data = 32'hA5B6C7D8;
-    cpu_request = 1;
-    read_request = 1;
-    #10;
-    show_state; // expect COMPARE
+        // 3. SCENARIU: CERERE DE CITIRE (READ REQUEST) de la CPU
+        @(negedge clk);
+        cpu_request = 1;
+        read_request = 1;
+        address = 32'h00002048; // Adresa din log-ul tău
 
-    #10;
-    show_state; // ramanem in COMPARE
+        // FSM ar trebui să treacă în COMPARE_TAG în următorul ciclu
+        @(negedge clk);
+        // Oprim cererea de la CPU dacă controller-ul a preluat adresa în registru
+        cpu_request = 0; 
+        
+        // Simulăm că tag-urile sunt aduse din Cache Array
+        // Presupunem un TAG diferit ca să forțăm un READ MISS (ex: toate tag-urile din cache sunt 0)
+        tags_from_cache = {19'd0, 19'd0, 19'd0, 19'd0}; 
+        tags_delivered = 1;
 
-    // Oprim request
-    cpu_request = 0;
-    read_request = 0;
-    #10;
-    show_state;
+        // În acest moment, FSM vede că e MISS și verifică dacă block-ul vechi e Dirty
+        // Lăsăm victim_dirty = 0, deci ar trebui să sară direct în starea READ MISS (011)
+        @(negedge clk);
+        tags_delivered = 0; // Resetăm semnalul de sincronizare cache
 
-    // Test 2 - WRITE request => trebuie sa mearga in COMPARE
-    reset = 1;
-    #20;
-    reset = 0;
-    #10;
-    show_state; // expect IDLE
+        // Suntem în READ MISS. Așteptăm ca memoria RAM să răspundă cu blocul de date
+        #30; 
+        
+        @(negedge clk);
+        block_delivered_ram = 1;
+        block_data_ram = {16{32'hAABBCCDD}}; // Umplem blocul cu o valoare test
 
-    data = 32'h12345678;
-    cpu_request = 1;
-    write_request = 1;
-    #10;
-    show_state; // expect COMPARE
+        // Cache-ul intern confirmă scrierea noului bloc adus din RAM
+        @(negedge clk);
+        block_delivered_ram = 0;
+        newBlock_delivered = 1;
 
-    #10;
-    show_state; // ramanem in COMPARE
+        // FSM ar trebui să se întoarcă în IDLE
+        @(negedge clk);
+        newBlock_delivered = 0;
 
-    cpu_request = 0;
-    write_request = 0;
-    #10;
-    show_state;
+        // Lăsăm simularea să ruleze puțin înainte de final
+        #50;
+        $stop;
+    end
 
-    // Test 3 - Reset in mijlocul operatiei
-    cpu_request = 1;
-    read_request = 1;
-    data = 32'hDEADBEEF;
-    #10;
-    show_state; // expect COMPARE
-    reset = 1;
-    #20;
-    show_state; // expect IDLE
-    reset = 0;
-    #10;
-    show_state; // expect IDLE
-
-    #20;
-    $finish;
-end
+    // --- Monitorizare în consolă ---
+    initial begin
+        $monitor("[CLK %0d] STATE=%b | REQ_TAGS=%b TAGS_DEL=%b REQ_BLK_RAM=%b REQ_BLK=%b DATA_READY=%b | addr=%h", 
+                 $time/10, uut.out_reg_current_state, request_memory_tags, tags_delivered, request_block_ram, request_block, data_ready, address);
+    end
 
 endmodule
